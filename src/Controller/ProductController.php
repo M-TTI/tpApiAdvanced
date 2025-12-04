4354eb77d2f709e3b1db1a0b78cfd9251b7e9d72
Martin Bonetti <martin.bonetti@ynov.com>
2025-12-04 15:19:06 +0100

    Resolved stash conflict, removed workspace.xml

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Services\ProductService;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
